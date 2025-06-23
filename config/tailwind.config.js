3512a7e9adfb36e6f66a2f58f87a535ea16948e0
mimi <snow.95j@gmail.com>
2025-06-23 18:28:41 +0900

    add:daisyUI導入

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
       },
     },
   },
-  plugins: [
+  plugins: [require("daisyui")
     // require('@tailwindcss/forms'),
     // require('@tailwindcss/typography'),
     // require('@tailwindcss/container-queries'),
