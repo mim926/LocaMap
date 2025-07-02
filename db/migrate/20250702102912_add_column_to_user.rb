379ac984330fcc0523ab5d171ee5335fabf52cdf
mimi <snow.95j@gmail.com>
2025-07-02 19:51:15 +0900

    add:Userモデルの追加カラムを記述(name等)

--- db/migrate/20250702102912_add_column_to_user.rb
+++ db/migrate/20250702102912_add_column_to_user.rb
@@ -1,4 +1,8 @@
 class AddColumnToUser < ActiveRecord::Migration[7.2]
   def change
+    add_column :users, :name, :string
+    add_column :users, :prefecture_id, :integer
+    add_column :users, :local_history, :string
+    add_column :users, :icon_image, :string
   end
 end
