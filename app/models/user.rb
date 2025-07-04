fb97d2046f76e56a5e4a7881147531d8ba60a56e
mimi <snow.95j@gmail.com>
2025-07-04 14:19:55 +0900

    fix:動作確認のため、一時的に都道府県が空欄でも許可とした

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  belongs_to :prefecture
+  belongs_to :prefecture, optional: true
 end
