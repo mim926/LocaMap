b5451dfd4ec4ca2684062bdbca40b9a89d92ce08
mimi <snow.95j@gmail.com>
2025-07-10 12:30:42 +0900

    fix:Lintエラー修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
+require "carrierwave/storage/abstract"
+require "carrierwave/storage/file"
+require "carrierwave/storage/fog"
 # ActiveSupport::Deprecation.warnの対応
 module ActiveSupport
   class Deprecation
@@ -12,13 +12,13 @@ end
 
 CarrierWave.configure do |config|
   config.storage :fog
-  config.fog_provider = 'fog/aws'
+  config.fog_provider = "fog/aws"
   config.fog_directory  = "locamap-images-bucket"
   config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: 'ap-northeast-1',
+    provider: "AWS",
+    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    region: "ap-northeast-1",
     path_style: true
   }
   config.fog_attributes = {}
