224630d0916abafc1a268eeb3536edc25967a91d
mimi <snow.95j@gmail.com>
2025-07-10 11:48:36 +0900

    fix:技術面談にてエラー対応

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,18 @@
 require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
+# ActiveSupport::Deprecation.warnの対応
+module ActiveSupport
+  class Deprecation
+    class << self
+      public :warn
+    end
+  end
+end
 
 CarrierWave.configure do |config|
   config.storage :fog
-  # config.fog_provider = 'fog/aws'
+  config.fog_provider = 'fog/aws'
   config.fog_directory  = "locamap-images-bucket"
   config.fog_credentials = {
     provider: 'AWS',
