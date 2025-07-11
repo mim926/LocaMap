2eac91e54a4a13552b494531b42c8cadab5e7354
mimi <snow.95j@gmail.com>
2025-07-11 17:46:56 +0900

    add:投稿削除機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
 
   get "home/index"
 
-  resources :posts, only: %i[index new create show edit update]
+  resources :posts, only: %i[index new create show edit update destroy]
 end
