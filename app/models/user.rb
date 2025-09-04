289b24356a9ea023652c968b7049a896b748163c
mimi <snow.95j@gmail.com>
2025-09-04 19:12:22 +0900

    add:コントローラー編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,16 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  def like(post)
+    like_posts << post
+  end
+
+  def unlike(post)
+    like_posts.destroy(post)
+  end
+
+  def like?(post)
+    like_posts.include?(post)
+  end
 end
