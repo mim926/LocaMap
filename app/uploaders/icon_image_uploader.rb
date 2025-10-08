0259a86b8de7533092e7f619dd6413228e82aa55
mimi <snow.95j@gmail.com>
2025-10-08 19:57:54 +0900

    fix:デフォルトアイコンをmap.japan.pngに変更

--- app/uploaders/icon_image_uploader.rb
+++ app/uploaders/icon_image_uploader.rb
@@ -15,7 +15,7 @@ class IconImageUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
-    "noimage.png"
+    "map.japan.png"
   end
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
