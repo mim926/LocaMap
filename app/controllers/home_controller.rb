f170b98cde5daa6a8c78a1a8b6573bb7dc7f460e
mimi <snow.95j@gmail.com>
2025-10-11 19:13:49 +0900

    add:ページネーション実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @q = Post.ransack(params[:q])
-    @posts = @q.result.includes(:user, :prefecture, :category).order(created_at: :desc)
+    @posts = @q.result.includes(:user, :prefecture, :category).order(created_at: :desc).page(params[:page]).per(3)
   end
 end
