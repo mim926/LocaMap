dbe851a1ae2aa05e8ff2f28339ae8f721354312e
mimi <snow.95j@gmail.com>
2025-10-26 18:00:07 +0900

    add:Googleログイン機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_09_25_084606) do
+ActiveRecord::Schema[7.2].define(version: 2025_10_17_150030) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -72,6 +72,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_09_25_084606) do
     t.integer "prefecture_id"
     t.string "local_history"
     t.string "icon_image"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
