3283764da94566fbf08b01572681f0c88d681dfd
mimi <snow.95j@gmail.com>
2025-10-28 21:32:30 +0900

    fix:デプロイ時のエラー修正

--- lib/action_mailer/sendgrid_delivery_method.rb
+++ lib/action_mailer/sendgrid_delivery_method.rb
@@ -1,22 +1,24 @@
 require "sendgrid-ruby"
 include SendGrid
 
-class SendGridDeliveryMethod
-  def initialize(options = {})
-    @api_key = options[:api_key] || ENV["SENDER_API_KEY"]
-  end
+module ActionMailer
+  class SendgridDeliveryMethod
+    def initialize(options = {})
+      @api_key = options[:api_key] || ENV["SENDER_API_KEY"]
+    end
 
-  def deliver!(mail)
-    from = Email.new(email: mail.from.first)
-    to = Email.new(email: mail.to.first)
-    content = Content.new(type: "text/html", value: mail.body.decoded)
-    subject = mail.subject
+    def deliver!(mail)
+      from = Email.new(email: mail.from.first)
+      to = Email.new(email: mail.to.first)
+      content = Content.new(type: "text/html", value: mail.body.decoded)
+      subject = mail.subject
 
-    sg_mail = Mail.new(from, subject, to, content)
-    sg = SendGrid::API.new(api_key: @api_key)
-    response = sg.client.mail._("send").post(request_body: sg_mail.to_json)
+      sg_mail = Mail.new(from, subject, to, content)
+      sg = SendGrid::API.new(api_key: @api_key)
+      response = sg.client.mail._("send").post(request_body: sg_mail.to_json)
 
-    Rails.logger.info "SendGrid API response: #{response.status_code}"
-    response
+      Rails.logger.info "SendGrid API response: #{response.status_code}"
+      response
+    end
   end
 end
