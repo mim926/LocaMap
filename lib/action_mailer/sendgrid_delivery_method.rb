b61a8674fba0369a6b605c47ff5cae9aa3a09f31
mimi <snow.95j@gmail.com>
2025-10-28 20:56:34 +0900

    fix:本番環境のメールが送れるように実装

---
+++ lib/action_mailer/sendgrid_delivery_method.rb
@@ -0,0 +1,22 @@
+require "sendgrid-ruby"
+include SendGrid
+
+class SendGridDeliveryMethod
+  def initialize(options = {})
+    @api_key = options[:api_key] || ENV["SENDER_API_KEY"]
+  end
+
+  def deliver!(mail)
+    from = Email.new(email: mail.from.first)
+    to = Email.new(email: mail.to.first)
+    content = Content.new(type: "text/html", value: mail.body.decoded)
+    subject = mail.subject
+
+    sg_mail = Mail.new(from, subject, to, content)
+    sg = SendGrid::API.new(api_key: @api_key)
+    response = sg.client.mail._("send").post(request_body: sg_mail.to_json)
+
+    Rails.logger.info "SendGrid API response: #{response.status_code}
+    response
+  end
+end
